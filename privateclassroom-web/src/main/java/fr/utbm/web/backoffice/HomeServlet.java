34914a8105fee86ac77680692b9a9f4f69a4d13e
fabiensamj <fabie.sam15121999@gmail.com>
2021-05-30 17:46:13 +0200

    ajout du web

---
+++ privateclassroom-web/src/main/java/fr/utbm/web/backoffice/HomeServlet.java
@@ -0,0 +1,56 @@
+package fr.utbm.web.backoffice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+//@WebServlet(name = "HomeServlet", urlPatterns= {"/homeServlet","/home.html"})
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	 protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        response.setContentType("text/html;charset=UTF-8");
+	        try ( PrintWriter out = response.getWriter()) {
+	            /* TODO output your page here. You may use following sample code. */
+	            out.println("<!DOCTYPE html>");
+	            out.println("<html>");
+	            out.println("<head>");
+	            out.println("<title>Dvd Store</title>");            
+	            out.println("</head>");
+	            out.println("<body>");
+	            out.println("<h1>Videostore - Shop Management online</h1>");
+	            out.println("<a href=/backoffice/list>Liste des films :</a>");
+	            out.println("<a href=/backoffice/addMovieForm.html>Ajouter un film</a>");
+	            out.println("</body>");
+	            out.println("</html>");
+	        }
+	    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+	}
+
+}
