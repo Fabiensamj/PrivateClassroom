c427af2d319eecfc02c7ed6e818e274188a07986
fabiensamj <fabie.sam15121999@gmail.com>
2021-06-02 01:06:55 +0200

    JSP exemple

--- privateclassroom-web/src/main/java/fr/utbm/web/servlet/ListServlet.java
+++ privateclassroom-web/src/main/java/fr/utbm/web/servlet/ListServlet.java
@@ -1,7 +1,6 @@
 package fr.utbm.web.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,8 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.utbm.core.entity.Location;
-import fr.utbm.core.service.LocationService;
+import fr.utbm.core.entity.Course;
+import fr.utbm.core.service.CourseService;
 
 /**
  * Servlet implementation class ListServlet
@@ -18,30 +17,18 @@ import fr.utbm.core.service.LocationService;
 public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
-	LocationService ls;
+	CourseService cs;
    
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.ls = new LocationService();
-        List<Location> listFilm = this.ls.getAllLocation();
+        this.cs = new CourseService();
+        List<Course> listCourse = this.cs.getAllCourse();
+        
+        String name = "test";
+        request.setAttribute("list", listCourse);
+        request.setAttribute("name", name);
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Tous les lieux</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Tous les lieux</h1>");
-            out.println("<ul>");
-            for(Location f : listFilm){
-                out.println("<li>" + f.getCity() +" - "+f.getId() +"<li>");
-            }
-            out.println("</ul>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/list.jsp" ).forward( request, response );
     }
 
 	
--- privateclassroom-web/src/main/webapp/WEB-INF/jsp/list.jsp
+++ privateclassroom-web/src/main/webapp/WEB-INF/jsp/list.jsp
@@ -1,12 +1,35 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+<%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">   
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta charset="UTF-8">
+<title>Private Classroom</title>
 </head>
 <body>
+<h1> Voici la liste des cours disponibles</h1>
 
+
+<c:forEach var="list" items="${list}">
+    <c:out value="${list.title}"/> = <c:out value="${list.code}"/></br>
+</c:forEach>
+
+
+<ul>
+<c:forEach var="i" begin="1" end="5" step="1">
+    <c:out value="${ name}" /><br/>
+</c:forEach>
+
+
+<%for(int i=0; i < 5; i++)
+		{
+		
+		%>
+		
+		<p><%= i %></p>
+	<%}%>
+</ul>
 </body>
 </html>
